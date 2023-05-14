78372762ae460a68515743bd016b89671f40f433
saw <doomtakersaw@gmail.com>
2023-05-14 19:30:37 +0630

    db_test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,33 @@
-import { type AppType } from "next/app";
+// import { type AppType } from "next/app";
 
+// import { api } from "~/utils/api";
+
+// import "~/styles/globals.css";
+// //import { ClerkProvider } from "@clerk/nextjs/dist/components.server";
+
+// const MyApp: AppType = ({ Component, pageProps }) => {
+//   return (
+//     //<ClerkProvider {...pageProps}>
+//       <Component {...pageProps} />
+//     //</ClerkProvider>
+//    );
+// };
+
+// export default api.withTRPC(MyApp);
+
+// page/_app.tsx
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
-
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
-
+ 
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
+ 
 export default api.withTRPC(MyApp);
+
